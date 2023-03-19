24702bf6c8f3de9b3b84536033592cd698d584cb
もᴋᴀɴᴀꜱʜɪᴍɪ炎 <92143243+LokyNoku@users.noreply.github.com>
2023-03-19 09:47:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,13 @@
-const player = document.querySelector('.player');
-const enemy = document.querySelector('.enemy');
-const scoreElement = document.querySelector('.score');
-let score = 0;
-
-function getRandom(min, max) {
-    return Math.random() * (max - min) + min;
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabpanel");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].className = tabcontent[i].className.replace(" active", "");
+  }
+  tablinks = document.getElementsByClassName("tab");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).className += " active";
+  evt.currentTarget.className += " active";
 }
-
-function moveEnemy() {
-    let target = getRandom(0, 90);
-    let time = getRandom(2000, 5000);
-
-    enemy.style.bottom = target + '%';
-
-    setTimeout(moveEnemy, time);
-}
-
-function checkCollision() {
-    let playerRect = player.getBoundingClientRect();
-    let enemyRect = enemy.getBoundingClientRect();
-
-    if (playerRect.bottom < enemyRect.top || playerRect.top > enemyRect.bottom || playerRect.right < enemyRect.left || playerRect.left > enemyRect.right) {
-        return false;
-    } else {
-        return true;
-    }
-}
-
-function startGame() {
-    moveEnemy();
-
-    setInterval(() => {
-        if (checkCollision()) {
-            alert('Você perdeu!');
-            location.reload();
-        }
-
-        score++;
-        scoreElement.textContent = score;
-    }, 1000 / 60);
-}
-
-startGame();
-
-document.addEventListener('touchstart', (event) => {
-    event.preventDefault();
-    let touchY = event.touches[0].clientY;
-
-    document.addEventListener('touchmove', (event) => {
-event.preventDefault();
-let deltaY = touchY - event.touches[0].clientY;
-    player.style.bottom = parseFloat(player.style.bottom) + deltaY / window.innerHeight * 100 + '%';
-
-    touchY = event.touches[0].clientY;
-});
-
